91aeb5996d806e9fdd21674969425cdb0152ea48
lefthandedgoat <lefthandedgoat@gmail.com>
2013-08-13 22:07:50 -0500

    books

--- acc2013/acc2013/Program.fs
+++ acc2013/acc2013/Program.fs
@@ -1,1 +1,15 @@
-﻿
+﻿open System
+
+let read = System.IO.File.ReadAllText
+let files = System.IO.Directory.GetFiles(@"c:\books\")
+
+files 
+|> Array.map read
+|> Array.fold(fun accumulatedBook book -> accumulatedBook + book) String.Empty
+|> fun combined -> combined.Split(' ')
+|> Seq.countBy id
+|> Seq.sortBy (fun (word, count) -> count)
+|> List.ofSeq
+|> List.rev
+|> Seq.take 10
+|> Seq.iter (fun (word, count) -> printfn "%s %i" word count)
